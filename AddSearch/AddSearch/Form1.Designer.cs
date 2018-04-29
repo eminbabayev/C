d57b3a0b4b3f794d800ba32254f0d78a9f90717c
Emin <30857069+eminbabayev@users.noreply.github.com>
2018-04-29 12:03:36 +0400

    Add/Search

---
+++ AddSearch/AddSearch/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace AddSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Tab = new System.Windows.Forms.TabPage();
+            this.btn = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Grid = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lbl = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.Tab.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.Tab);
+            this.tabControl1.Location = new System.Drawing.Point(56, 29);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(239, 393);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btn2);
+            this.tabPage1.Controls.Add(this.textBox3);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(231, 367);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Search";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.Turquoise;
+            this.btn2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn2.Location = new System.Drawing.Point(18, 138);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(188, 70);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "AXTAR";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(18, 29);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(188, 20);
+            this.textBox3.TabIndex = 0;
+            this.textBox3.TextChanged += new System.EventHandler(this.txt_TextChanged);
+            // 
+            // Tab
+            // 
+            this.Tab.Controls.Add(this.btn);
+            this.Tab.Controls.Add(this.dateTimePicker1);
+            this.Tab.Controls.Add(this.textBox2);
+            this.Tab.Controls.Add(this.textBox1);
+            this.Tab.Location = new System.Drawing.Point(4, 22);
+            this.Tab.Name = "Tab";
+            this.Tab.Padding = new System.Windows.Forms.Padding(3);
+            this.Tab.Size = new System.Drawing.Size(231, 367);
+            this.Tab.TabIndex = 1;
+            this.Tab.Text = "Add";
+            this.Tab.UseVisualStyleBackColor = true;
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(19, 237);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(181, 49);
+            this.btn.TabIndex = 3;
+            this.btn.Text = "Elave et";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(19, 99);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(181, 20);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(19, 162);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(181, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(19, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Grid
+            // 
+            this.Grid.AllowUserToAddRows = false;
+            this.Grid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.Grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Grid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.Grid.Location = new System.Drawing.Point(340, 51);
+            this.Grid.Name = "Grid";
+            this.Grid.Size = new System.Drawing.Size(467, 364);
+            this.Grid.TabIndex = 1;
+            this.Grid.Click += new System.EventHandler(this.txt_TextChanged);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Ad Soyad";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Dogum gunu";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Telefon";
+            this.Column3.Name = "Column3";
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(380, 6);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(0, 13);
+            this.lbl.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(841, 459);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.Grid);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.Tab.ResumeLayout(false);
+            this.Tab.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage Tab;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView Grid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ AddSearch/AddSearch/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddSearch
+{
+    public partial class Form1 : Form
+    {
+        List<DataGridView> dt = new List<DataGridView>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" && textBox2.Text == "")
+            {
+                lbl.Text = "Duzgun daxil et";
+                lbl.ForeColor = Color.Blue;
+
+            }
+            else
+            {
+                DateTime date = dateTimePicker1.Value;
+                Grid.Rows.Add(textBox1.Text, dateTimePicker1.Text, textBox2.Text);
+                textBox1.Text = "";
+                textBox2.Text = "";
+                dateTimePicker1.Value = DateTime.Now;
+            }
+
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Grid.Rows.Count; i++)
+            {
+                if (Grid.Rows[i].Visible)
+                {
+                    for (int j = 0; j < Grid.Rows[i].Cells.Count; j++)
+                    {
+                        MessageBox.Show(Grid.Rows[i].Cells[j].Value.ToString());
+                    }
+                }
+
+            }
+
+        }
+        private void txt_TextChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Grid.Rows.Count; i++)
+            {
+                for (int j = 0; j < Grid.Rows[i].Cells.Count; j++)
+                {
+                    if (Grid.Rows[i].Cells[j].Value.ToString().Contains(textBox3.Text))
+                    {
+                        Grid.Rows[i].Visible = true;
+                        break;
+
+
+                    }
+                    else
+                    {
+                        Grid.Rows[i].Visible = false;
+                    }
+                }
+            }
+
+        }
+    }
+}
