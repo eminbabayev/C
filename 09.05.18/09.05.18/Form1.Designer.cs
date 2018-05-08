91b1e3e7d3b91b617f8b6b4f84df4a886b45fdbc
Emin <30857069+eminbabayev@users.noreply.github.com>
2018-05-09 01:36:08 +0400

    C# + SQL

---
+++ 09.05.18/09.05.18/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace _09._05._18
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DtgStudents = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtName = new System.Windows.Forms.TextBox();
+            this.TxtSurname = new System.Windows.Forms.TextBox();
+            this.TxtPhone = new System.Windows.Forms.TextBox();
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.LblError = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DtgStudents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DtgStudents
+            // 
+            this.DtgStudents.AllowUserToAddRows = false;
+            this.DtgStudents.AllowUserToDeleteRows = false;
+            this.DtgStudents.AllowUserToOrderColumns = true;
+            this.DtgStudents.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DtgStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DtgStudents.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.DtgStudents.Location = new System.Drawing.Point(319, 75);
+            this.DtgStudents.Name = "DtgStudents";
+            this.DtgStudents.RowTemplate.Height = 24;
+            this.DtgStudents.Size = new System.Drawing.Size(460, 273);
+            this.DtgStudents.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Id";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Name";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Surname";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Phone";
+            this.Column4.Name = "Column4";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Surname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Phone";
+            // 
+            // TxtName
+            // 
+            this.TxtName.Location = new System.Drawing.Point(12, 59);
+            this.TxtName.Name = "TxtName";
+            this.TxtName.Size = new System.Drawing.Size(148, 22);
+            this.TxtName.TabIndex = 4;
+            // 
+            // TxtSurname
+            // 
+            this.TxtSurname.Location = new System.Drawing.Point(12, 149);
+            this.TxtSurname.Name = "TxtSurname";
+            this.TxtSurname.Size = new System.Drawing.Size(148, 22);
+            this.TxtSurname.TabIndex = 5;
+            // 
+            // TxtPhone
+            // 
+            this.TxtPhone.Location = new System.Drawing.Point(12, 260);
+            this.TxtPhone.Name = "TxtPhone";
+            this.TxtPhone.Size = new System.Drawing.Size(148, 22);
+            this.TxtPhone.TabIndex = 6;
+            // 
+            // BtnAdd
+            // 
+            this.BtnAdd.Location = new System.Drawing.Point(12, 316);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(147, 45);
+            this.BtnAdd.TabIndex = 7;
+            this.BtnAdd.Text = "Add";
+            this.BtnAdd.UseVisualStyleBackColor = true;
+            this.BtnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // LblError
+            // 
+            this.LblError.AutoSize = true;
+            this.LblError.Location = new System.Drawing.Point(26, 405);
+            this.LblError.Name = "LblError";
+            this.LblError.Size = new System.Drawing.Size(0, 17);
+            this.LblError.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LblError);
+            this.Controls.Add(this.BtnAdd);
+            this.Controls.Add(this.TxtPhone);
+            this.Controls.Add(this.TxtSurname);
+            this.Controls.Add(this.TxtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DtgStudents);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DtgStudents)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DtgStudents;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtName;
+        private System.Windows.Forms.TextBox TxtSurname;
+        private System.Windows.Forms.TextBox TxtPhone;
+        private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.Label LblError;
+    }
+}
+
---
+++ 09.05.18/09.05.18/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace _09._05._18
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            FillStudents();
+        }
+        private void FillStudents()
+        {
+            DtgStudents.Rows.Clear();
+            SqlConnection con = new SqlConnection(@"Server = AZCNGRM\SQLEXPRESS; Database = School; Integrated Security = SSPI;");
+            con.Open();
+            string query = "SELECT * FROM Students";
+            SqlCommand command = new SqlCommand(query, con);
+            SqlDataReader reader = command.ExecuteReader();
+            //MessageBox.Show(reader.HasRows.ToString());
+
+            while (reader.Read())
+            {
+                DtgStudents.Rows.Add(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3));
+
+            }
+
+            con.Close();
+            
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            LblError.Text = "";
+            string Name = TxtName.Text;
+            string Surname = TxtSurname.Text;
+            string Phone = TxtPhone.Text;
+
+            if(Name == String.Empty || Surname == String.Empty || Phone == String.Empty)
+            {
+                LblError.Text = "Bosluq daxil etmeyin";
+                return;
+            }
+
+            SqlConnection con = new SqlConnection(@"Server = AZCNGRM\SQLEXPRESS; Database = School; Integrated Security = SSPI;");
+            con.Open();
+            string query = "INSERT INTO Students([Name],[Surname],[Phone]) VALUES('" + Name + "', '" + Surname + "', '" + Phone + "')";
+
+            SqlCommand command = new SqlCommand(query, con);
+            int affRow = command.ExecuteNonQuery();
+            con.Close();
+
+            FillStudents();
+            TxtName.Text = "";
+            TxtSurname.Text = "";
+            TxtPhone.Text = "";
+
+
+        }
+    }
+}
